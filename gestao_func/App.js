1cb9f60fa1b3118c383e0e4488b992c41a5d9e01
eliances.github.io <eliancesar.p@gmail.com>
2024-03-14 21:17:31 -0300

    instalação das bibliotecas + início do projeto no App.js

---
+++ gestao_func/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>INFORMAÇÕES DE CONTATO</Text>
+      <StatusBar style="auto" />
+      <Text>Nome:</Text>
+      <TextInput></TextInput>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
